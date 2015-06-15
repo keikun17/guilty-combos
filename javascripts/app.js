e0ded5593f735921042cbe4c40f45e55299934bf
Buddy Magsipoc <keikun17@gmail.com>
2015-06-16 02:45:15 +0800

    Site updated to 37b1b5b

--- javascripts/app.js
+++ javascripts/app.js
@@ -25166,8 +25166,8 @@ var GuiltyCombosApp = React.createClass({displayName: "GuiltyCombosApp",
   render: function() {
 
     var combo_data = [
-      {name:"Dust Loop", combo_string: ['P','.4', 'K', '.3','S', 'HS', '.7', 'P K S', '↓ ↘ →'] },
-      {name:"Gunflame FRC", combo_string: ['↓ ↘ → P', '.6', 'P K S', '.2', '↓ ↘ → K', '↓ ↘ → P', '.6', 'P K S',  '.2', '↓ ↘ → K'] }
+      {name:"Dust Loop", combo_string: ['→ P','.4', 'K', '.4','↓ HS', '.6' ,'↗ D', '.6','D', '.6','→→ ↗ D' ,'.6', 'D','.8','↗ D' ,'.6', 'D', '.6', '→→ ↗ S', '.4', 'D', '.3','↗ S', '.3', 'D', '.3', '→ ↓ ↘ HS', '.3','↓ ↙ ← K'] },
+      {name:"Gunflame FRC Loop", combo_string: ['↓ ↘ → P', '.6', 'P K S', '.4', '↓ ↘ → K', '1','↓ ↘ → P', '.6', 'P K S',  '.4', '↓ ↘ → K'] }
     ];
 
     var combos = combo_data.map(function(combo , index){
