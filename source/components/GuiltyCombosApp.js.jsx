c2b48d3593209855b95f74c156ae864667a26d79
Buddy Magsipoc <keikun17@gmail.com>
2015-06-15 18:48:38 +0800

    Combo refactor : use data。

--- source/components/GuiltyCombosApp.js.jsx
+++ source/components/GuiltyCombosApp.js.jsx
@@ -6,11 +6,21 @@
 
 var GuiltyCombosApp = React.createClass({
   render: function() {
+
+    var combo_data = [
+      {name:"Dust Loop", combo_string: ['P', 'K', 'S', 'HS', 'P K S', '↓ ↘ →'] },
+      {name:"Gunflame FRC", combo_string: ['↓ ↘ → P', 'P K S'] }
+    ];
+
+
+    var combos = combo_data.map(function(combo , index){
+      return <Combo name={combo.name} combo_string={combo.combo_string} key={index} />
+    })
+
     return (
       <div className='main'>
         <ReactTransitionGroup transitionName="fade">
-          <Combo name="Dust Loop" combo_string={['P', 'K', 'S', 'HS', 'P K S', '↓ ↘ →']} />
-          <Combo name="Gunflame FRC" combo_string={['↓ ↘ → P', 'P K S']} />
+          {combos}
         </ReactTransitionGroup>
       </div>
     );
