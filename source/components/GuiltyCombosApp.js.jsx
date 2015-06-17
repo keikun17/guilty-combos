6bd3fe48a2e1ef601245853bbc9eee873d91358f
Buddy Magsipoc <keikun17@gmail.com>
2015-06-17 21:10:13 +0800

    Dust loop to fafner。

--- source/components/GuiltyCombosApp.js.jsx
+++ source/components/GuiltyCombosApp.js.jsx
@@ -9,6 +9,7 @@ var GuiltyCombosApp = React.createClass({
 
     var combo_data = [
       {name:"Dust Loop", combo_string: ['→ P','.4', 'K', '.4','↓ HS', '.6' ,'↗ D', '.6','D', '.6','→→ ↗ D' ,'.6', 'D','.8','↗ D' ,'.6', 'D', '.6', '→→ ↗ S', '.4', 'D', '.3','↗ S', '.3', 'D', '.3', '→ ↓ ↘ HS', '.3','↓ ↙ ← K'] },
+      {name:"Dust Loop -> Fafner", combo_string: ['→ P','.4', 'K', '.4','↓ HS', '.6' ,'↗ D', '.6','D', '.7','← ↙ ↓ ↘ → HS', '.4','→→ →P', '.3', '↓ HS', '.3', '↗ D', '.6', 'D'] },
       {name:"Gunflame FRC Loop", combo_string: ['↓ ↘ → P', '.6', 'P K S', '.4', '↓ ↘ → K', '1','↓ ↘ → P', '.6', 'P K S',  '.4', '↓ ↘ → K'] }
     ];
 
